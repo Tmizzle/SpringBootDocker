9fa8f7203ea07337b0cb5615afe126c79f783567
Tmizzle <aleksandarplanic21@gmail.com>
2023-02-13 15:00:48 +0100

    Initial commit

---
+++ spring-boot-example/src/main/java/com/alex/Main.java
@@ -0,0 +1,47 @@
+package com.alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping(path = "api/v1/customers")
+public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(String name, String email, Integer age) {
+    }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+    @PutMapping(" {customerId} ")
+    public void updateStudent(@PathVariable("customerId") Integer studentId,
+                              @RequestParam(required = false) String name,
+                              @RequestParam(required = false) String email) {
+        updateStudent(studentId, name, email);
+    }
+}
